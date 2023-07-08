81ffd64da9c63cb92457359e9c90174bac5f69c9
Andrew Smith <abominablesnowmanxo@gmail.com>
2023-07-08 23:38:06 +0400

    customuser model added

--- backend/smm_portal/settings.py
+++ backend/smm_portal/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Local
+    'accounts.apps.AccountsConfig',
     'projects.apps.ProjectsConfig',
 ]
 
@@ -114,3 +115,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
